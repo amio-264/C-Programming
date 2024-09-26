f7105bed42ce0dd8e77760ab87219ed36d2461e4
amio-264 <amiosarkar264@gmail.com>
2024-09-27 02:54:04 +0600

    Storing Variables

---
+++ variables.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a = 5;
+    float pi = 3.1416;
+    char star = '*';
+    int age = 21;
+    printf("%d\n%.4f\n%c\n%d", a, pi, star, age);
+    return 0;
+}

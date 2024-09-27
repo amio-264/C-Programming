3b24c63b204b3d319a8b23cf4c2768b0112fe999
amio-264 <amiosarkar264@gmail.com>
2024-09-27 18:05:43 +0600

    Data type conversion

---
+++ Chapter2/Type_conversion.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    printf("Sum of 2 and 3 is %d\n", 2 + 3);//int op int = int
+    printf("Sum of 2 and 3 is %f\n", 2.0 + 3);//int op float = float
+    printf("Sum of 2 and 3 is %f\n", 2.0 + 3.0);//float op float =float
+    return 0;
+}

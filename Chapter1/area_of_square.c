d3ad0dbb1c1499bafc70f8d6b335d3b3b0d6e4ec
amio-264 <amiosarkar264@gmail.com>
2024-09-27 14:10:41 +0600

    Area of square

--- Chapter1/area_of_square.c
+++ Chapter1/area_of_square.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 int main()
+//area of the square
 {
     float a;
     printf("Enter the side of the square ");
-    scanf("%f", &a);
-    printf("The area of the square is %f", a * a);
+    scanf("%f", &a);//Taking input from user
+    printf("The area of the square is %f", a * a);//Printing the result
     return 0;
 }

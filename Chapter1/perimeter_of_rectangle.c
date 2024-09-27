b5b5775ac3605b84b9319f782f347446213fae81
amio-264 <amiosarkar264@gmail.com>
2024-09-27 14:13:16 +0600

    Perimeter of rectangle

--- Chapter1/perimeter_of_rectangle.c
+++ Chapter1/perimeter_of_rectangle.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 int main()
+//perimeter of rectangle
 {
     float a, b;
     printf("Enter length and width of the rectangle ");
-    scanf("%f%f", &a, &b);
-    printf("The perimeter of Rectangle is %f", 2 * (a + b));
+    scanf("%f%f", &a, &b);//Taking input from user
+    printf("The perimeter of Rectangle is %f", 2 * (a + b));//Printing the result
     return 0;
 }

6c16fb40a55c9d586115b76b61dc3cbc569ff74b
amio-264 <amiosarkar264@gmail.com>
2024-09-27 13:36:10 +0600

    Sum of two numbers

--- sum_of_two_number.c
+++ sum_of_two_number.c
@@ -3,7 +3,7 @@ int main()
 //C Program for sum of two numbers
 {
     int a, b, sum;
-    printf("Enter two number ");
+    printf("Enter two numbers ");
     scanf("%d%d", &a, &b); //taking input from user
     sum = a + b;//performing the sum
     printf("The sum is %d", sum);//printing the output

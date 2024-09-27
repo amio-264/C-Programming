40478446175eaa1a4e85aef708a444c3223194a3
amio-264 <amiosarkar264@gmail.com>
2024-09-27 14:05:02 +0600

    Usage of different variables

--- Chapter1/variables.c
+++ Chapter1/variables.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 int main()
+//Usage of different variables in C
 {
     int a = 5;
     float pi = 3.1416;
     char star = '*';
     int age = 21;
-    printf("%d\n%.4f\n%c\n%d", a, pi, star, age);
+    printf("%d\n%.4f\n%c\n%d\n", a, pi, star, age);
     return 0;
 }

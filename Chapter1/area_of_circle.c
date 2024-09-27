c509584159a3a2d5416cbd7cc5f41ffcd4e7f560
amio-264 <amiosarkar264@gmail.com>
2024-09-27 23:06:45 +0600

    Area of circle

--- Chapter1/area_of_circle.c
+++ Chapter1/area_of_circle.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 #define PI 3.1416
 int main()
-//area of the circle
+// area of the circle
 {
     float r;
     printf("Enter the radius of the circle ");
-    scanf("%f", &r);//Taking input from user
-    printf("The area of the circle is %f", PI * r * r);//Printing the area
+    scanf("%f", &r);                                    // Taking input from user
+    printf("The area of the circle is %f", PI * r * r); // Printing the area
     return 0;
 }

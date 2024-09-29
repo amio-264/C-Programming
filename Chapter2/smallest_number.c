d431ac713f6bd1dd65bcda8748d8e2f7cdb0ece3
amio-264 <amiosarkar264@gmail.com>
2024-09-29 23:48:20 +0600

    Smallest number between two

--- Chapter2/smallest_number.c
+++ Chapter2/smallest_number.c
@@ -4,9 +4,11 @@ int main()
     int a, b;
     printf("Enter two number: ");
     scanf("%d", &a);
-    (!(a < b) && printf("The smallest number is: %d\n", b)) ||
-        ((a < b) && printf("The smallest number is: %d\n", a));
+    ((a < b) && printf("The smallest number is: %d\n", a)) ||
+        ((b <= a) && printf("The smallest number is: %d\n", b)); //(b <= a && printf("The smallest number is: %d\n", b)): If the first condition is false, this part will execute, printing b. The || ensures that one of the two will be executed.
     return 0;
+
+    
     // int a, b;
     // printf("Enter two number: ");
     // scanf("%d%d", &a, &b);

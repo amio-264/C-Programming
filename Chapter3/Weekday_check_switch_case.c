0dee3a63692a30964ca1df55b2ee22ab6288407b
amio-264 <amiosarkar264@gmail.com>
2024-10-02 01:14:23 +0600

    Switch-Case statement

--- Chapter3/Weekday_check_switch_case.c
+++ Chapter3/Weekday_check_switch_case.c
@@ -1,41 +1,42 @@
+// Switch Case Syntax switch (variable)
+// {
+// case C1:
+//     do something
+//     break;
+// case C2:
+//     do something
+//     break;
+// default:
+//     do something
+//     break;
+// }
 #include <stdio.h>
 int main()
 {
-    /*
-    Switch Case Syntax
-    switch(variable)
-    {
-    case C1:do something
-    break;
-    case C2:do something
-    break;
-    default:do something
-    }
-    */
     int day; // 1-Sat;2-Sun;3-Mon;4-Tues;5-Wed;6-Thurs;7-Fri
     printf("Enter day (1-7): ");
     scanf("%d", &day);
     switch (day)
     {
-    case 1 :
+    case 1:
         printf("Saturday\n");
         break;
-    case 2 :
+    case 2:
         printf("Sunday\n");
         break;
-    case 3 :
+    case 3:
         printf("Monday\n");
         break;
-    case 4 :
+    case 4:
         printf("Tuesday\n");
         break;
-    case 5 :
+    case 5:
         printf("Wednesday\n");
         break;
-    case 6 :
+    case 6:
         printf("Thursday\n");
         break;
-    case 7 :
+    case 7:
         printf("Friday\n");
         break;
     default:

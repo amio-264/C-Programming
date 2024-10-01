c2e01b022cd955edc258530b4f4c608710031a98
amio-264 <amiosarkar264@gmail.com>
2024-10-01 22:50:07 +0600

    Age_check

--- Chapter3/age_check.c
+++ Chapter3/age_check.c
@@ -4,13 +4,18 @@ int main()
     int age;
     printf("Enter age: ");
     scanf("%d", &age);
-    if (age > 18)
+    if (age >= 18)
     {
         printf("Adult\n");
     }
+    else if (age>=13 && age<18)
+    {
+        printf("Teenager\n");
+    }
+    
     else
     {
-        printf("Not adult\n");
+        printf("Child\n");
     }
     return 0;
 }

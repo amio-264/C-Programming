b15fc82f3e50849dfa64bb3f21da8549cba6b576
amio-264 <amiosarkar264@gmail.com>
2024-10-03 00:15:52 +0600

    Float char input

---
+++ Chapter4/float_char_print.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    for (float i = 0.0; i <= 10.0; i++)
+    {
+        printf("%.2f \n", i);
+    }
+    for (char j = 'A'; j <= 'Z'; j++)
+    {
+        printf("%c \n", j);
+    }
+    return 0;
+}

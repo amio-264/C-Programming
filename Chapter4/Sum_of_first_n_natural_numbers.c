f3f9b32599b8cf519562f2c4d3577b3c77b820b9
amio-264 <amiosarkar264@gmail.com>
2024-10-03 03:26:30 +0600

    Sum of first n numbers

--- Chapter4/Sum_of_first_n_natural_numbers.c
+++ Chapter4/Sum_of_first_n_natural_numbers.c
@@ -4,15 +4,28 @@ int main()
     int n, sum = 0;
     printf("Enter a number: ");
     scanf("%d", &n);
-    for (int i = 1; i <= n; i++)
-    {
-        sum += i; // sum = sum + i
-    }
-    printf("Sum is %d\n", sum);
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     sum += i; // sum = sum + i
+    // }
+    // printf("Sum is %d", sum);
 
-    for (int i = n; i >= 1; i--)
+    // for (int i = n; i >= 1; i--)
+    // {
+    //     printf("%d\n", i);
+    // }
+
+    // for (int j = n; j >= 1; j--)
+    // {
+    //     sum += j;
+    //     printf("%d\n", j);
+    // }
+    // printf("The sum is %d\n", sum);
+    for (int i = 1, j = n; i <= n && j >= 1; i++, j--)
     {
-        printf("%d\n", i);
+        sum += i;
+        printf("%d\n", j);
     }
+    printf("Sum is %d\n",sum);
     return 0;
 }

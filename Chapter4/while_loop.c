a23f9e7b9b5f427e211acfb9baa809ea20ea020a
amio-264 <amiosarkar264@gmail.com>
2024-10-03 00:25:43 +0600

    While loop

---
+++ Chapter4/while_loop.c
@@ -0,0 +1,22 @@
+// Syntax for While loop
+// variable declaration
+// while(condition)
+// {
+//     do something;
+//     updation;
+// }
+/*
+for(int i=0;i<=5;i++)
+Code for the loop in while is below
+*/
+#include <stdio.h>
+int main()
+{
+    int i = 0;
+    while (i <= 5)
+    {
+        printf("%d\n", i);
+        i++;
+    }
+    return 0;
+}

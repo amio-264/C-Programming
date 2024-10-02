c313b6d510116f4f47e7763c25a6e8741f7add25
amio-264 <amiosarkar264@gmail.com>
2024-10-03 00:32:22 +0600

    Print zero to nth number

--- Chapter4/PQ_14.c
+++ Chapter4/PQ_14.c
@@ -4,11 +4,18 @@ int main()
     int n;
     printf("Enter the number ");
     scanf("%d", &n);
-    int i = 0;
-    while (i <= n)
+    
+    // int i = 0;
+    // while (i <= n)
+    // {
+    //     printf("%d\n", i);
+    //     i++;
+    // }
+
+    for (int i = 0; i <= n; i++)
     {
         printf("%d\n", i);
-        i++;
     }
+
     return 0;
 }

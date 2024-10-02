55c98a227a34e496fb60d32b5386734adca2ffce
amio-264 <amiosarkar264@gmail.com>
2024-10-03 03:54:38 +0600

    natural number test

---
+++ Chapter3/natural_number.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<math.h>
+int main()
+{
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    if(n<1){
+        printf("Enter a natural number");
+    }
+    else{
+        printf("Natural number uoto %d are:\n", n);
+        for (int i = 1; i <= n;i++){
+            printf("%d\n", i);
+        }
+       // printf("\n");
+    }
+    return 0;
+}

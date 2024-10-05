4f33150c0725ab6a934e13acd63b94914a344b9b
amio-264 <amiosarkar264@gmail.com>
2024-10-06 02:04:31 +0600

    Diamond pattern

---
+++ Chapter4/pattern7.c
@@ -0,0 +1,37 @@
+// Merge of two Right angle triangle in right hand side(Diamond Pattern)
+#include <stdio.h>
+int main()
+{
+    int n, i, j; // i = row ; j = column
+    printf("Enter the number of row: ");
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++)
+    {
+        // printing space
+        for (j = 1; j <= n - i; j++)
+        {
+            printf(" ");
+        }
+        // printing number
+        for (j = 1; j <= i; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    for (i = n - 1; i >= 1; i--)
+    {
+        // printing space
+        for (j = 1; j <= n - i; j++)
+        {
+            printf(" ");
+        }
+        // printing number
+        for (j = 1; j <= i; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}

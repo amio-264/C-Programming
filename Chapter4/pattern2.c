abb1b5d8fcc86caadf2d4b67e61b6a0e8af852c8
amio-264 <amiosarkar264@gmail.com>
2024-10-05 22:06:00 +0600

    Right angle triangle where row values are printed

---
+++ Chapter4/pattern2.c
@@ -0,0 +1,15 @@
+// Right Angle Triangle Pattern
+#include <stdio.h>
+int main()
+{
+    int n, i, j; // i = row and j = column
+    printf("Enter the number of rows: ");
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= i; j++)
+        printf("%d", i); //printing the value of row only
+        printf("\n");
+    }
+    return 0;
+}

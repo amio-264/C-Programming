d3305cbc1be1bbb7307cb9b9fb629539cba41070
amio-264 <amiosarkar264@gmail.com>
2024-10-05 22:36:03 +0600

    Added diffferent types

--- Chapter4/pattern2.c
+++ Chapter4/pattern2.c
@@ -9,9 +9,63 @@ int main()
     {
         for (j = 1; j <= i; j++)
         {
-            printf("%d", i); // printing the value of row only
+            printf("%d ", i); // printing the value of row only
         }
         printf("\n");
     }
     return 0;
 }
+
+ // Right Angle Triangle Pattern
+// #include <stdio.h>
+// int main()
+// {
+//     int n, i, j; // i = row and j = column
+//     printf("Enter the number of rows: ");
+//     scanf("%d", &n);
+//     for (i = 1; i <= n; i++)
+//     {
+//         for (j = 1; j <= i; j++)
+//         {
+//             printf("%d ", i % 2); // printing the value of row only in binary
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+// Right Angle Triangle Pattern
+// #include <stdio.h>
+// int main()
+// {
+//     int n, i, j; // i = row and j = column
+//     printf("Enter the number of rows: ");
+//     scanf("%d", &n);
+//     for (i = 1; i <= n; i++)
+//     {
+//         for (j = 1; j <= i; j++)
+//         {
+//             printf("%d ", i + 64); // printing the value of row only which is A-Z
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+// Right Angle Triangle Pattern
+// #include <stdio.h>
+// int main()
+// {
+//     int n, i, j; // i = row and j = column
+//     printf("Enter the number of rows: ");
+//     scanf("%d", &n);
+//     for (i = 1; i <= n; i++)
+//     {
+//         for (j = 1; j <= i; j++)
+//         {
+//             printf("* ",); // printing the value of row only which is any special character
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }

1f8007c56d5e307d52787d87d807248927c91afe
amio-264 <amiosarkar264@gmail.com>
2024-10-06 13:12:09 +0600

    modified

--- Chapter4/pattern13.c
+++ Chapter4/pattern13.c
@@ -18,5 +18,21 @@ int main()
         }
             printf("\n");
     }
+    for (i = n - 1; i >= 1; i--)
+    {
+        for (j = 1; j <= n - i; j++)
+        {
+            printf("  ");
+        }
+        for (j = 1; j <= i; j++)
+        {
+            printf("%d ", j);
+        }
+        for (j = i - 1; j >= 1; j--)
+        {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
     return 0;
 }

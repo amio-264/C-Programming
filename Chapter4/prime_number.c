76d226cdaaed678bc1fea4d65fe710098e76cf5a
amio-264 <amiosarkar264@gmail.com>
2024-10-06 15:51:18 +0600

    prime number

--- Chapter4/prime_number.c
+++ Chapter4/prime_number.c
@@ -4,12 +4,16 @@ int main()
     int n, count = 1; // declaring a variable "count = 1" means its initialized to being a prime number.
     printf("Enter a positive number: ");
     scanf("%d", &n);
+    if (n <= 1) // condition checks that 1 or zero can't be prime number
+    {
+        printf("%d is not a prime number", n);
+    }
     for (int i = 2; i < n; i++) // Check the condition if the number is divided by n to n-1
     {
         if (n % i == 0)
         {
             count = 0; // not a prime number
-            break; // exit loop if the number is divided by other numbers except 1 and n
+            break;     // exit loop if the number is divided by other numbers except 1 and n
         }
     }
     if (count) // used count variable in this condition to check if the number is prime or not

a04a5c7129aa6f689aaf8f2d376992da6b29d8c6
amio-264 <amiosarkar264@gmail.com>
2024-10-07 17:53:48 +0600

    Function Basics

--- Chapter5/function_basic.c
+++ Chapter5/function_basic.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 //function declaration/prototype
 void printHello();
+void printGoodBye();
+
 int main()
 {
-    printHello();
     printHello(); // function call
-    printHello();
+    printGoodBye();
     return 0;
 }
 
 // function definition
 void printHello(){
     printf("Hello!\n");
-    printf("My name is Amio Sarkar\n");
+    printf("My name is Amio Sarkar.\n");
+}
+
+void printGoodBye(){
+    printf("Good bye!\n");
 }

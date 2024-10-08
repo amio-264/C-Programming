328d7acdd6bc8a3a3ef970ccd2f62bfef37f6158
amio-264 <amiosarkar264@gmail.com>
2024-10-08 20:57:23 +0600

    practice

--- Chapter6/practice.c
+++ Chapter6/practice.c
@@ -3,15 +3,15 @@ int main()
 {
     int *ptr;
     int x;
-    ptr = &x;
-    *ptr = 0;
-    printf("x = %d\n", x);
-    printf("*ptr = %d\n", *ptr);
-    *ptr += 5;
-    printf("x = %d\n", x);
-    printf("*ptr = %d\n", *ptr);
-    (*ptr)++;
-    printf("x=%d\n", x);
-    printf("*ptr=%d\n", *ptr);
+    ptr = &x; // ptr variable stores address of value
+    *ptr = 0; // value at address which is x = 0
+    printf("x = %d\n", x); // 0
+    printf("*ptr = %d\n", *ptr); // 0
+    *ptr += 5; // x = 5   
+    printf("x = %d\n", x); // 5
+    printf("*ptr = %d\n", *ptr); // 5
+    (*ptr)++; // x = 6
+    printf("x=%d\n", x); // 6
+    printf("*ptr=%d\n", *ptr); // 6
     return 0;
 }

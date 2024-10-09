2f350af69bd61e5a54d8c32ebe299c2492b6d5f6
amio-264 <amiosarkar264@gmail.com>
2024-10-09 17:31:53 +0600

    Initialization of array

---
+++ Chapter7/initialization_array.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    float price[3] = {100.0, 200.0, 300.0}; // initialization of array
+    printf("Total price 1: %f\n", price[0] + (.18 * price[0]));
+    printf("Total price 2: %f\n", price[1] + (.18 * price[1]));
+    printf("Total price 3: %f\n", price[2] + (.18 * price[2]));
+    return 0;
+}

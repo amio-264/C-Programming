dae9b14212f05df6e29cc8046ca2df8759b1d666
amio-264 <amiosarkar264@gmail.com>
2024-10-16 23:22:23 +0600

    storing table in an array

--- Chapter7/storing_table_of_2_3.c
+++ Chapter7/storing_table_of_2_3.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-void storeTable(int arr[][10], int x, int y, int n)
+void storeTable(int arr[][10], int row, int col, int number)
 {
-    for (int i = 0; i < y; i++){
-        arr[x][i] = n * (i + 1);
+    for (int i = 0; i < col; i++)
+    {
+        arr[row][i] = number * (i + 1);
     }
 }
 int main()
@@ -10,15 +11,11 @@ int main()
     int table[2][10];
     storeTable(table, 0, 10, 2);
     storeTable(table, 1, 10, 3);
-
     for (int i = 0; i < 10; i++)
     {
         printf("%d\t", table[0][i]);
     }
-
     printf("\n");
-
-    
     for (int i = 0; i < 10; i++)
     {
         printf("%d\t", table[1][i]);

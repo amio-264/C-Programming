a506b0f2a0f7bccdaffe0817307cff3eb0299571
amio-264 <amiosarkar264@gmail.com>
2024-12-20 01:35:02 +0600

    slicie using string

---
+++ Chapter8/slice.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+void slice(char str[], int n, int m){
+    char newStr[100];
+    int j = 0;
+    for (int i = n; i <= m; i++, j++){
+        newStr[j] = str[i];
+    }
+    newStr[j] = '\0';
+    puts(newStr);
+}
+int main()
+{
+    char str[100] = "Amiosarkar";
+    slice(str, 3, 6);
+    return 0;
+}
